d4587f0a03e65b4cf377cb862658d01ea21ba931
Julienne Matias <julienne.m93@gmail.com>
2018-10-16 11:09:26 +0200

    Mise en application du routeur vers les différentes sections du menu

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const ROUTES: Routes = [
+    { path: 'user', component: UserProfileComponent },
+    { path: 'sign-up', component: SignUpComponent},
+];
+
+export { ROUTES };
