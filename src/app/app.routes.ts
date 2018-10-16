63214acf7e4bf1b2f5644c69b53ba278f136e49a
Julienne Matias <julienne.m93@gmail.com>
2018-10-16 17:03:33 +0200

    Correction route initiale

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,9 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const ROUTES: Routes = [
-    { path: 'user', component: UserProfileComponent },
-    { path: 'sign-up', component: SignUpComponent},
+    { path: 'user', component: UserProfileComponent, pathMatch: 'full' },
+    { path: '', component: UserProfileComponent, pathMatch: 'full' },
+    { path: 'sign-up', component: SignUpComponent, pathMatch: 'full' },
 ];
 
 export { ROUTES };
